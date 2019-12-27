702720ba9f7138db57a31a701990fb28c74bc819
rohinirp <rohini2501rp@gmail.com>
2019-12-27 08:43:19 +0530

    create personal button

---
+++ src/PersonalButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class PersonalButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Personal Button</div>;
+  }
+}
+export default PersonalButton;
