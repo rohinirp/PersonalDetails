b43bfa161158ee92dbf75c41f29a69719863dfc3
rohinirp <rohini2501rp@gmail.com>
2020-01-06 09:19:23 +0530

    validationls

--- src/Login.js
+++ src/Login.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import PersonalText from "./PersonalText";
 import PersonalDropdwn from "./PersonalDropdwn";
 import PersonalButton from "./PersonalButton";
-var menuItem = [100 , 200 , 300]
+var menuItem = ["Html", "Css", "Javascript", "Reactjs"];
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       user: "",
-      pass: "" ,
-      Drpvalue : ''
+      pass: "",
+      Drpvalue: ""
     };
   }
   changeUser = e => {
@@ -24,19 +24,59 @@ class Login extends Component {
     });
   }
   signUp = () => {
-    alert("sign up");
+    var user = this.state.user;
+    var pass = this.state.pass;
+    var Drpvalue = this.state.Drpvalue;
+    if (user === "" || user.length <= 0) {
+      alert("Please fill the user name");
+      return;
+    }
+    if (pass === "" || pass.length <= 0) {
+      alert("please fill the password");
+      return;
+    }
+    if (Drpvalue === "" || Drpvalue.length <= 0) {
+      alert("Please select the course");
+      return;
+    }
+
+    var Tot = [];
+    if (localStorage.getItem("Register")) {
+      var data = JSON.parse(localStorage.getItem("Register"));
+      Tot.push(data);
+    }
+    var data = {
+      UserName: user,
+      Password: pass,
+      PersonalDropdwn: Drpvalue
+    };
+    Tot.push(data);
+    console.log(data);
+    var FinalData = JSON.stringify(Tot);
+
+    localStorage.setItem("Register", FinalData);
+
+    this.setState({
+      user: "",
+      pass: "",
+      Drpvalue: ""
+    });
   };
   signIn = () => {
+    var data = localStorage.getItem("Register");
+    var NewData = JSON.parse(data);
+    console.log(data);
+    console.log(NewData);
     if (this.state.user === this.state.pass) {
       alert("success");
     } else {
       alert("Failed");
     }
   };
-  changeDrpval(e){
+  changeDrpval(e) {
     this.setState({
-      Drpvalue : e.target.value
-    })
+      Drpvalue: e.target.value
+    });
   }
   render() {
     return (
@@ -51,7 +91,11 @@ class Login extends Component {
           value={this.state.pass}
           onChange={e => this.changePassword(e)}
         />
-        <PersonalDropdwn value={this.state.Drpvalue} onChange={(e)=>this.changeDrpval(e)} menuData={menuItem}/>
+        <PersonalDropdwn
+          value={this.state.Drpvalue}
+          onChange={e => this.changeDrpval(e)}
+          menuData={menuItem}
+        />
         <PersonalButton PersonalBtnValue={"Sign Up"} onClick={this.signUp} />
         <PersonalButton PersonalBtnValue={"Sign In"} onClick={this.signIn} />
       </div>
